6626b72b21188c023a43234e0feb798bcd7668d0
abdalrahman seria <abdalrahmanseria2000@gmail.com>
2026-05-11 22:02:29 +0300

    last

--- Tqreerk-backend/obj/Debug/net8.0/Tqreerk-backend.AssemblyInfo.cs
+++ Tqreerk-backend/obj/Debug/net8.0/Tqreerk-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tqreerk-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f7f16a77a178e85ac5ef2202ecce2fe6ba857dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faef3d3f4b232a8f84645d86ce9a2b4490050197")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tqreerk-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tqreerk-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
